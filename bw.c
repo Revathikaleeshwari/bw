a008eca50a213fe71aabfe3c4869a9e14c844e4e
Revathi N <nrevathibe38@gmail.com>
2016-07-06 10:16:13 +0530

    Create bw.c

---
+++ bw.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int a(const void*d,const void*n)
+{
+return (*(char*)n)_(*(char*)d)
+void main()
+int i,j,n;
+char str[2*10]
+scanf("%d",&n);
+j=n;
+w(i<n)
+{
+i=0;
+str[i]='B';
+str[j+1]='w';
+j++;
+}
+str[n*2]='\0';
+qsort(str,2*n,sizeof(char),a);
+printf("%s",str);
+return 0;
+}
